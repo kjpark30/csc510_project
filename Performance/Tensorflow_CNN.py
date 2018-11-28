c25bcba71839b3b2af222ce0958836a56099a79a
kjpark30 <38594114+kjpark30@users.noreply.github.com>
2018-11-28 15:57:51 -0500

    Update Tensorflow_CNN.py

--- Performance/Tensorflow_CNN.py
+++ Performance/Tensorflow_CNN.py
@@ -78,7 +78,7 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 1024]
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
-  # Add dropout operation; 0.6 probability that element will be kept
+  # Add dropout operation; 0.5 probability that element will be kept
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.5, training=mode == tf.estimator.ModeKeys.TRAIN)
 
